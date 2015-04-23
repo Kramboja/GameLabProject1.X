e9e0eba0834d85d9469f72d881e3ba59399adef0
Kramboja <koen5067@icloud.com>
2015-04-23 11:02:27 +0200

    oculus importet

--- gameLabWeek1/Assets/_Scripts/dieScript.cs
+++ gameLabWeek1/Assets/_Scripts/dieScript.cs
@@ -3,17 +3,31 @@ using System.Collections;
 
 public class dieScript : MonoBehaviour {
 
+	public Material[] states;
+	private int lives;
+
 	// Update is called once per frame
-	void Update () 
+	void Awake () 
 	{
-	
+		lives = 3;
+
+		GetComponent<Renderer>().material = states[lives - 1];
 	}
 
 	void OnCollisionEnter(Collision collision)
 	{
 		if(collision.gameObject.tag != "plane" && collision.gameObject.tag != "enemy")
 		{
-			Destroy(gameObject);
+			Debug.Log(lives);
+
+			if(lives < 0);
+			{
+				Destroy(gameObject);
+			}
+
+			lives -= 1;
+
+			GetComponent<Renderer>().material = states[lives - 1];
 		}
 	}
 }
