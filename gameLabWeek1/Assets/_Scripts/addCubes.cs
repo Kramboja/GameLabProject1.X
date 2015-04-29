6be07269b88031c96575b3223f0729ceb909abf1
Kramboja <koen5067@icloud.com>
2015-04-29 09:46:23 +0200

    AddingBlocksInTheMenu

---
+++ gameLabWeek1/Assets/_Scripts/addCubes.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class addCubes : MonoBehaviour {
+
+	public GameObject block;
+	
+	private int blockCounter;
+	private bool interactable;
+
+	void Start()
+	{
+		interactable = true;
+		addBlocks();
+	}
+
+	public void addBlocks()
+	{
+		if(interactable)
+		{
+			Instantiate(block,new Vector3(200f,10f,5f),Quaternion.identity);
+			blockCounter++;
+		}
+		interactable = false;
+		
+		Invoke("handlingCheck",0.3f);
+	}
+	
+	private void handlingCheck()
+	{
+		interactable = true;
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		addBlocks();
+	}
+}
